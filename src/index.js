f7b9299d93797c689af7718735e3092a4477eae3
WilintonAscanio <92041487+WilintonAscanio@users.noreply.github.com>
2023-02-06 08:28:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+    res.header('Access-Control-Allow-Methods', '*')
     next()
 
 })
